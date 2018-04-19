28d4e09ab3a4de4c4d543cd12b3e14a3188aecf0
Randall Harris Haynes <harrishaynes@gmail.com>
2018-04-19 17:42:21 +0000

    automatically backed up by learn

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -4,22 +4,19 @@ class ArtistsController < ApplicationController
   def index
     @artists = Artist.all
   end
-
-  def show
-  end
-
+  
   def new
     @artist = Artist.new
   end
 
   def create
     @artist = Artist.new(artist_params)
-
-    if @artist.save
-      redirect_to @artist
-    else
-      render :new
+    if !!@artist.save then redirect_to @artist
+    else render :new
     end
+  end
+
+  def show
   end
 
   def edit
