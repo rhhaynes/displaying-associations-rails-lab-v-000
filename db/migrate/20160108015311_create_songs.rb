b365f86286724664d7bb31b2987c68d8fffe8fad
Randall Harris Haynes <harrishaynes@gmail.com>
2018-04-19 17:08:49 +0000

    automatically backed up by learn

--- db/migrate/20160108015311_create_songs.rb
+++ db/migrate/20160108015311_create_songs.rb
@@ -1,7 +1,8 @@
 class CreateSongs < ActiveRecord::Migration
   def change
     create_table :songs do |t|
-      t.string :title
+      t.string  :title
+      t.integer :artist_id
 
       t.timestamps null: false
     end
